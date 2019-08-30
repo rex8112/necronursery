fb434b52031c60f1925e0dd1fd817ec3939ab82e
Riley Winkler <rex8112@gmail.com>
2019-08-30 16:15:20 -0500

    Add the beginning portions of detecting whether you touch a Grave
    It's currently untested but should theoretically work
    
    Signed-off-by: Riley Winkler <rex8112@gmail.com>

---
+++ Assets/Scripts/graveController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class graveController : MonoBehaviour
+{
+    Vector3 touchPosWorld;
+
+    TouchPhase touchPhase = TouchPhase.Ended;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == touchPhase)
+        {
+            touchPosWorld = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+
+            Vector2 touchPosWorld2D = new Vector2(touchPosWorld.x, touchPosWorld.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
+
+            if (hit.collider != null)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
