e63bc083784ed037f1fa3b60f2e10f5924f48539
Concha <drew.concha@trojans.dsu.edu>
2019-09-03 16:24:31 -0500

    this commit will add scene changes and scripts for flesh

---
+++ Assets/Scripts/menuButtons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class menuButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void changeSceneByName(string name)
+    {
+        SceneManager.LoadScene(name);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/resourceQuantities.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class resourceQuantities : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Text fleshCountBar;
+    public int fleshCount = 0;
+    public int fleshCap = 10;
+
+    void Start()
+    {
+        fleshCount = 0;
+        //fleshCountBar = gameObject.GetComponent<Text>();
+        fleshCountBar.text = fleshCount.ToString(); //+ "/" + fleshCap.ToString(); 
+    }
+
+    public void fleshManagement(int change)
+    {
+        change = 2;
+        fleshCount += change;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
