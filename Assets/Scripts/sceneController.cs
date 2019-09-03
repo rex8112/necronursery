b08976805729c62d27dcd9257277743a97231cce
Riley Winkler <rex8112@gmail.com>
2019-09-03 15:54:10 -0500

    Add sceneController to control camera movement

---
+++ Assets/Scripts/sceneController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneController : MonoBehaviour
+{
+    public Vector2 touchStart;
+    public Vector2 touchDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    touchStart = touch.position;
+                    break;
+                case TouchPhase.Moved:
+                    touchDir = touch.position - touchStart;
+                    moveCamera(touchDir);
+                    break;
+            }
+        }
+    }
+
+    void moveCamera(Vector2 dir)
+    {
+        Transform cam = Camera.main.transform;
+
+        Debug.Log(dir);
+        cam.Translate(-dir/4);
+    }
+}
