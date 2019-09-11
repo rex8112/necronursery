10f4ca597eed9926a6ab21cea955727490a61b1a
Concha <drew.concha@trojans.dsu.edu>
2019-09-11 15:07:51 -0500

    add begginging test to changing prefabs for growing stages

---
+++ Assets/Scripts/prefabChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class prefabChange : MonoBehaviour
+{
+
+    public Button test;
+    public Sprite second;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        test = GetComponent<Button>();
+
+        
+    }
+    public void changeImage()
+    {
+        test.GetComponent<Image>().sprite = second;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
