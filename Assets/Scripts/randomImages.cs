4cce21a3b266107f9bf893ff58b173afc8b6c873
Brooke Gronli <brooke.gronli@trojans.dsu.edu>
2019-09-18 08:53:14 -0500

    add the start to body parts in the morgue

---
+++ Assets/Scripts/randomImages.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomImages : MonoBehaviour
+{
+    //public List<Object> images;
+    SpriteRenderer sr;
+    int count = 5;
+    int index;
+    Sprite[] images = new Sprite[5];
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        fillListOfSprites();
+        showRandomImage();
+    }
+
+    void showRandomImage()
+    {
+        index = Random.Range(0, count);
+        sr.sprite = images[index];
+    }
+
+    void fillListOfSprites()
+    {
+        if(images)
+        images = Resources.Load<Sprite>("Sprites");
+    }
+
+}
