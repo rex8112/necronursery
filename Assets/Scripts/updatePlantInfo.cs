f92579311004ab6275f14cd5497f84d77faa762a
Concha <drew.concha@trojans.dsu.edu>
2019-09-23 14:57:26 -0500

    added starting code for tend to plant button

---
+++ Assets/Scripts/updatePlantInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class updatePlantInfo : MonoBehaviour
+{
+
+    [SerializeField] resourceManager resourceManager;
+    [SerializeField] plantManager plantManager;
+
+    public Button giveResource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+        Button btn = giveResource.GetComponent<Button>();
+        btn.onClick.AddListener(inventoryChanges);
+
+    }
+
+    public void inventoryChanges()
+    {
+
+
+        //if(resourceManager.Resource(gameObject.GetComponent<string>) == )
+        {
+
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+    }
+}
