51147176d9295c91f693d21f4d40e15dd599bebe
Riley Winkler <rex8112@gmail.com>
2019-10-17 16:23:56 -0500

    Fix morgue element locations and experimenting with trash placement

--- Assets/Scripts/MinigamesScripts/Morgue/morgueController.cs
+++ Assets/Scripts/MinigamesScripts/Morgue/morgueController.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class morgueController : MonoBehaviour
 {
     public GameObject bodyPart; //bodyPart Prefab
     public GameObject trash; //Trash Prefab
+    public GameObject bag;
+    public RectTransform dumpster;
+    public Canvas mainCanvas;
     public GameObject[] allParts; //An array of all the bodyparts in the scene
     public List<SpriteRenderer> wantedParts; //List of the parts the player needs to find
-    SpriteRenderer spriteSwitch; //The sprite that is getting set (for what the player needs to find)
+    Image spriteSwitch; //The sprite that is getting set (for what the player needs to find)
     SpriteRenderer chosenOne; //Holds the sprite before the spriteSwitch
     public int bodyPartCount = 6; //The number of bodyParts that will spawn
     public int trashCount = 10; //How many trash will spawn
@@ -17,6 +21,8 @@ public class morgueController : MonoBehaviour
     float randomY; //The random Y location of a body part
     float randomX; //The random X location of a body part
     int test = 0;
+
+
 
     void Awake()
     {
@@ -32,6 +38,18 @@ public class morgueController : MonoBehaviour
         MakeGameObjects();
         allParts = GameObject.FindGameObjectsWithTag("bodypart");
         WantedBodyParts();
+    }
+
+    private void Update() //Positions the bag
+    {
+        Vector3 worldPoint = new Vector3();
+        Camera cam = Camera.main;
+        float height = cam.pixelHeight / 2;
+        float bagWidth = bag.transform.lossyScale.x;
+        worldPoint = cam.ScreenToWorldPoint(new Vector3(0, height));
+        worldPoint.x += bagWidth + 1;
+        worldPoint.z = 0f;
+        bag.transform.position = worldPoint;
     }
 
     void WantedBodyParts() //Makes a list of bodyParts that the player needs to find
@@ -50,13 +68,14 @@ public class morgueController : MonoBehaviour
             {
                 wantedParts.Add(allParts[partNumber].GetComponent<SpriteRenderer>());
                 chosenOne = wantedParts[r];
-                spriteSwitch = GameObject.Find("Object" + r).GetComponent<SpriteRenderer>();
+                spriteSwitch = GameObject.Find("Object" + r).GetComponent<Image>();
                 spriteSwitch.sprite = chosenOne.sprite;
+                spriteSwitch.color = new Color(1, 1, 1, 1);
                 allParts[partNumber] = null; //setting the location to NULL so it won't be picked twice
             }
         }
-        GameObject.Find("Bag").GetComponent<morgueWin>().wantedPartsList = wantedParts;
-        GameObject.Find("Bag").GetComponent<morgueWin>().winAmount = randomAmount;
+        bag.GetComponent<morgueWin>().wantedPartsList = wantedParts;
+        bag.GetComponent<morgueWin>().winAmount = randomAmount;
     }
 
     void MakeGameObjects() //Create gameobjects in the scene
@@ -77,8 +96,9 @@ public class morgueController : MonoBehaviour
 
     void RandomLocation() //pick a random location
     {
-            randomY = Random.Range(-3.0f, 1.5f);
-            randomX = Random.Range(-5.0f, 5.0f);
+        float[] v = spawnRange();
+        randomY = Random.Range(v[1], v[3]);
+        randomX = Random.Range(v[0], v[2]);
     }
 
     public void ResetMinigame(bool reset)
@@ -90,6 +110,23 @@ public class morgueController : MonoBehaviour
             allParts = GameObject.FindGameObjectsWithTag("bodypart");
             WantedBodyParts();
         }
+    }
+
+    private float[] spawnRange()
+    {
+        Rect fixedRect;
+        fixedRect = RectTransformUtility.PixelAdjustRect(dumpster, mainCanvas);
+        Vector2 bottomL = new Vector2(fixedRect.x, fixedRect.y);
+        Vector2 topR = new Vector2(fixedRect.x + fixedRect.width, fixedRect.y + fixedRect.height);
+        Vector2 worldBottom = Camera.main.ScreenToWorldPoint(bottomL);
+        Vector2 worldTop = Camera.main.ScreenToWorldPoint(topR);
+        float[] values = new float[4];
+        values[0] = worldBottom.x;
+        values[1] = worldBottom.y;
+        values[2] = worldTop.x;
+        values[3] = worldTop.y;
+        Debug.Log(dumpster.sizeDelta.x + " " + dumpster.sizeDelta.y);
+        return values;
     }
 }
 
