7c2f5a45b67224fb323a649a0768a6f1b33d9337
Riley Winkler <rex8112@gmail.com>
2019-11-13 14:56:31 -0600

    Prevent constant tap to start
    Co-Authored-By: dtconcha <dtconcha@users.noreply.github.com>

--- Assets/Scripts/sceneController.cs
+++ Assets/Scripts/sceneController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine.Events;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class sceneController : MonoBehaviour
@@ -20,6 +21,7 @@ public class sceneController : MonoBehaviour
     [SerializeField] float tranSpeed = 6f;
     [SerializeField] float sizeSpeed = 1f;
     [SerializeField] GameObject AudioController;
+    [SerializeField] GameObject tap;
 
     [Space(10)]
     [SerializeField] List<graveController> graves = new List<graveController>();
@@ -36,11 +38,19 @@ public class sceneController : MonoBehaviour
     private void Start()
     {
         Load();
+        if (SaveLoad.tapToStart == false)
+        {
+            tap.SetActive(false);
+            gameObject.GetComponent<CamController>().HardTransition();
+        }
+        else
+            SaveLoad.tapToStart = false;
     }
 
     private void OnApplicationQuit()
     {
         Save();
+        SaveLoad.tapToStart = true;
     }
 
     private void OnApplicationPause(bool pause)
