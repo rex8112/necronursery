3d339a15fec5099febf728cdb0b8e25512f5d493
Riley Winkler <rex8112@gmail.com>
2019-11-14 15:44:08 -0600

    Inititalize BrewingController.cs
    Co-Authored-By: dtconcha <dtconcha@users.noreply.github.com>

---
+++ Assets/Scripts/MinigamesScripts/BrewingMama/BrewingController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrewingController : MonoBehaviour
+{
+    [SerializeField] GameObject cauldron;
+    [SerializeField] Image mainIngredient;
+    [SerializeField] GameObject cover;
+    [SerializeField] List<Sprite> ingredients;
+    
+
+}
