03c173e72d3d575dea524a7ccedd5bb8f791fbc7
dtconcha <42611457+dtconcha@users.noreply.github.com>
2019-11-14 10:49:19 -0600

    start tutorial dialogue

---
+++ Assets/Scripts/tutorialDialogue.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class tutorialDialogue : MonoBehaviour
+{
+    public GameObject tutBox;
+    public Text tutText;
+
+    public bool textActive;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
