dc16994b1a4741fdeed205df17d583bd67cdf3be
Riley Winkler <rex8112@gmail.com>
2019-11-15 08:45:14 -0600

    Add tileflip coroutine
    Co-Authored-By: dtconcha <dtconcha@users.noreply.github.com>

--- Assets/Scripts/MinigamesScripts/BrewingMama/BrewingController.cs
+++ Assets/Scripts/MinigamesScripts/BrewingMama/BrewingController.cs
@@ -9,6 +9,39 @@ public class BrewingController : MonoBehaviour
     [SerializeField] Image mainIngredient;
     [SerializeField] GameObject cover;
     [SerializeField] List<Sprite> ingredients;
-    
 
+    private List<GameObject> children = new List<GameObject>();
+
+    private void Start()
+    {
+        foreach (Transform c in cover.transform)
+            children.Add(c.gameObject);
+
+        StartCoroutine("TileFlip");
+    }
+
+    IEnumerator TileFlip()
+    {
+        while (true)
+        {
+            int indx = Random.Range(0, children.Count);
+            Image tmp = children[indx].GetComponent<Image>();
+
+            while (tmp.color.a > 0)
+            {
+                Color t = tmp.color;
+                t.a -= 0.1f;
+                tmp.color = t;
+                yield return new WaitForSeconds(0.05f);
+            }
+            while (tmp.color.a < 1)
+            {
+                Color t = tmp.color;
+                t.a += 0.1f;
+                tmp.color = t;
+                yield return new WaitForSeconds(0.05f);
+            }
+            yield return new WaitForSeconds(.1f);
+        }
+    }
 }
