8666bd9c0b8adf891c00a700d0da6e1671bcc3f2
Carl J. Petersen <25103835+cjpetersen@users.noreply.github.com>
2019-11-19 15:53:41 -0600

    Add tutorial dialog to the menu
    Add tutorial dialog to the menu on the grazeyard scene
    
    Co-Authored-By: XxX_Bi11y_XxX <twilightpuma@users.noreply.github.com>

--- Assets/Scripts/Dialogue/tutorialDialogue.cs
+++ Assets/Scripts/Dialogue/tutorialDialogue.cs
@@ -16,7 +16,7 @@ public class tutorialDialogue : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ShowBox(" Hello there young one. Planning to raise the dead are you? Good to see such spirits from today’s youth. Hehehe.");
+        ShowBox(" Hello there young one. Planning to raise the dead are you? Good to see such spirit from today’s youth. Hehehe.");
     }
 
     // Update is called once per frame
@@ -43,29 +43,27 @@ public class tutorialDialogue : MonoBehaviour
 
             switch(stepCounter)
             {
-                case 1: 
-                    tutText.text = " Hello there young one. Planning to raise the dead are you? Good to see such spirits from today’s youth. Hehehe.";
-                    break;
-                case 2:
+                
+                case 1:
                     tutText.text = "Ah, but you can’t raise such spirits without some tools. Thankfully our art isn’t as complex as it once was.";
                     break;
-                case 3:
+                case 2:
                     tutText.text = "Ah, the days of ceremonies and exotic sacrifices. Today it’s much simpler, and faster, to raise the dead. Or even create monsters.";
                     break;
-                case 4:
+                case 3:
                     tutText.text = "Ah, but to raise a monster, you’ll have to plant it. While we use to rely on dead bodies, today we have handy, and easy to move, seeds instead. Here have some, as a gift.";
                     break;
-                case 5:
+                case 4:
                     tutText.text = "You’ll need some ‘sacrifices’ to raise that seed to maturity. Here is some flesh for it.";
                     break;
-                case 6:
+                case 5:
                     tutText.text = "Flesh is a common enough resource. Easily obtained, and easy to feed to a monster plant.";
                     break;
-                case 7:
+                case 6:
                     tutText.text = "With that much Flesh, you should be able to raise a zombie from this seedling.Just don’t let the little fella nip any digits off your hand.They can be a bit feisty at first.";
                     break;
-                case 8:
-                    Destroy(gameObject);
+                case 7:
+                gameObject.SetActive(false);
                     break;
             }
 
