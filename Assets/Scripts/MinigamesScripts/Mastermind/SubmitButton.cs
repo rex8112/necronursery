796f04c500cd0fc207aebf2e8d12512a6a9c2fef
ladyflyers29 <42610874+ladyflyers29@users.noreply.github.com>
2020-01-23 14:58:57 -0600

    Add the beginning of number of tries to mastermind
    Co-Authored-By: jackson-augspurger <jackson-augspurger@users.noreply.github.com>

--- Assets/Scripts/MinigamesScripts/Mastermind/SubmitButton.cs
+++ Assets/Scripts/MinigamesScripts/Mastermind/SubmitButton.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SubmitButton : MonoBehaviour
 {
+    public int triesLeft;
+    public Text onScreenNumber;
 
+    private void Start()
+    {
+        triesLeft = 12;
+
+    }
     public void OnMouseDown()
     {
-        GameObject.Find("Main Camera").GetComponent<MastermindController>().CodeChecker(true);
+ //       GameObject.Find("Main Camera").GetComponent<MastermindController>().CodeChecker(true);
+        triesLeft -= 1;
+        onScreenNumber.text = (triesLeft).ToString("0");
     }
 
 
