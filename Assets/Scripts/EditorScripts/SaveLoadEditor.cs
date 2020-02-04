60e00d2064d6c682f7a2ede36ca3ce608a940ce2
Riley Winkler <rex8112@gmail.com>
2020-02-04 12:40:37 -0600

    Reorganize Editor scripts

---
+++ Assets/Scripts/EditorScripts/SaveLoadEditor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SaveLoad))]
+public class SaveLoadEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        SaveLoad saveLoad = (SaveLoad)target;
+        saveLoad.tapToStart = EditorGUILayout.Toggle("Tap To Start", saveLoad.tapToStart);
+        EditorGUILayout.BeginVertical("Box");
+        EditorGUILayout.LabelField("Player Variables", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField("XP To Level Up", saveLoad.xpToLevel.ToString());
+        saveLoad.level = EditorGUILayout.IntField("Level", saveLoad.level);
+        saveLoad.xp = EditorGUILayout.FloatField("XP", saveLoad.xp);
+        saveLoad.xpPerLevel = EditorGUILayout.FloatField("XP Per Level", saveLoad.xpPerLevel);
+        EditorGUILayout.EndVertical();
+
+        EditorGUILayout.BeginVertical("Box");
+        EditorGUI.indentLevel++;
+        EditorGUILayout.LabelField("Save Variables", EditorStyles.boldLabel);
+        string path = "resources";
+        EditorGUILayout.PropertyField(serializedObject.FindProperty(path), true);
+        path = "seeds";
+        EditorGUILayout.PropertyField(serializedObject.FindProperty(path), true);
+        path = "plants";
+        EditorGUILayout.PropertyField(serializedObject.FindProperty(path), true);
+        path = "stageInts";
+        EditorGUILayout.PropertyField(serializedObject.FindProperty(path), true);
+        EditorGUI.indentLevel--;
+        EditorGUILayout.EndVertical();
+    }
+}
